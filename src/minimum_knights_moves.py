26ed20782aafd0453ff81c491f8531b33cc51154
mrgerman <germanlozickyj2003@gmail.com>
2023-05-10 18:47:43 -0300

    problem solved

---
+++ src/minimum_knights_moves.py
@@ -0,0 +1,72 @@
+def bfs(root):
+    if not root : return []
+    response = [root]
+    queue = [root]
+    counter = 1
+    while queue:
+        for _ in range(len(queue)):
+            currentNode = queue.pop(0)
+            response.append(currentNode)
+            for child in currentNode.children:
+                queue.append(child)
+        counter+=1
+    return response
+
+"""
+En un tablero de ajedrez infinito tienes un caballo en la casilla [origenX, origenY].
+
+Un caballo tiene 8 posibles movimientos para hacer.
+
+Debes devolver el número mínimo necesario de movimientos para llevar al caballo a la casilla [objetivoX, objetivoY]. 
+
+Se garantiza que la respuesta sí existe.
+
+Ejemplo:
+
+# Input
+origenX = 0
+origenY = 0
+objetivoX = 5
+objetivoY = 5
+minKnightMoves(origenX, origenY, objetivoX, objetivoY)
+
+# Output
+4
+"""
+def moveHorsesInChess(origen_x : int, origen_y : int, objetivo_x : int, objetivo_y : int):
+    if origen_x == objetivo_x and origen_y == objetivo_y:
+        return 0
+    
+    movements = [
+        [1,2],[1,-2],[-1,2],[-1,-2],
+        [2,1],[2,-1],[-2,1],[-2,-2]
+    ]
+    queue = [[origen_x,origen_y]]   
+    cache = [[origen_x,origen_y]]
+    counter = 0
+
+    while queue :
+        for _ in range(len(queue)):
+            current_position = queue.pop(0)
+            if current_position[0] == objetivo_x and current_position[1] == objetivo_y :
+                return counter
+            if cache[0] == current_position[0] and cache[1] == current_position[1]: continue
+            cache.append([
+                    current_position[0],
+                    current_position[1]
+                ])
+            
+            for movement in movements:
+                x = current_position[0] + movement[0]
+                y = current_position[1] + movement[1]
+                if not abs(x - objetivo_x) < 8 and not abs(y - objetivo_x) < 8: continue
+                queue.append([
+                    current_position[0] + movement[0], 
+                    current_position[1] + movement[1]
+                ])
+        counter+=1
+        
+    return counter
+                
+
+print(moveHorsesInChess(0, 0, 5, 5))
---
+++ src/rotting_orange.py
@@ -0,0 +1,50 @@
+"""
+Vas a recibir una cuadrícula m x n en la que cada celda puede tener uno de los tres valores siguientes:
+
+0: representa una celda vacía
+1: representa una naranja fresca
+2: representa una naranja podrida
+Cada día cualquier naranja 4-direccionalmente adyacente a una naranja podrida también se vuelve podrida.
+
+Debes retornar el número mínimo de días que deben transcurrir hasta que ninguna celda tenga una naranja fresca. Si esto es imposible, retorna -1.
+
+Por ejemplo:
+
+// Input
+const cultivo = [
+  [2,1,1],
+  [1,1,0],
+  [0,1,1],
+];
+
+orangesRotting(cultivo);
+
+// Output
+4
+"""
+
+def oranges_rotting(crop):
+    queue = [[0, 0]]
+    counter = 0
+    while queue:
+          current_crop = queue.pop()
+          for y in range(len(crop)):
+               for x in range(len(crop[current_crop[0]])):
+                  if crop[y][x] == 2:
+                    crop[y][x] = 3
+                    current_count = 0
+                    if y + 1 < len(crop) and crop[ y + 1][x] == 1 : 
+                      current_count+=1
+                      crop[y+1][x] = 2 
+                    if x+1 < len(crop[0]) and crop[y][x+1] == 1:
+                      current_count+=1
+                      crop[y][x+1] = 2
+                    if not current_count == 0:
+                      counter+=1
+    return counter
+
+print(oranges_rotting([
+  [2,1,1],
+  [1,1,0],
+  [0,1,1],
+]))
