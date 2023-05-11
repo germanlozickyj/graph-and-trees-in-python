e332da7e5f99d0afcf721c2082f9aa30635d6429
mrgerman <germanlozickyj2003@gmail.com>
2023-05-10 21:57:23 -0300

    adding issues

--- src/shortest_bridge_between_islands.py
+++ src/shortest_bridge_between_islands.py
@@ -1,3 +1,40 @@
+"""
+Dada una matriz binaria n x m en la que 1 representa tierra y 0 agua, asumiendo que hay dos islas en el mapa, retorna el tamaño del puente más corto para conectar ambas islas.
+
+Recuerda que una isla está rodeada de agua y se forma conectando celdas con tierra adyacentes. Puedes suponer que los cuatro bordes de la cuadrícula están rodados de agua.
+
+Ejemplo 1:
+
+# Input
+mapa = [
+  [1,1,1,1,1],
+  [1,0,0,0,1],
+  [1,0,1,0,1],
+  [1,0,0,0,1],
+  [1,1,1,1,1],
+]
+Bridge().shortestBridge(mapa)
+
+# Output
+1
+
+Ejemplo 2:
+
+# Input
+mapa = [
+  [1,1,0,0,1],
+  [1,1,0,0,1],
+  [1,0,0,0,1],
+  [1,0,0,0,1],
+  [1,0,0,1,1],
+]
+Bridge().shortestBridge(mapa)
+
+# Output
+2
+"""
+
+
 class Bridge:
   def shortestBridge(self, A):
     # Tu código aquí 👇
