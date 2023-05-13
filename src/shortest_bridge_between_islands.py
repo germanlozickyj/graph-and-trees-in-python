02b05dbd2cdd9796d2aea2d4540c3e00edcf73ec
mrgerman <germanlozickyj2003@gmail.com>
2023-05-13 16:59:59 -0300

    problem fixed

--- src/shortest_bridge_between_islands.py
+++ src/shortest_bridge_between_islands.py
@@ -1,7 +1,9 @@
 """
-Dada una matriz binaria n x m en la que 1 representa tierra y 0 agua, asumiendo que hay dos islas en el mapa, retorna el tamaño del puente más corto para conectar ambas islas.
+Dada una matriz binaria n x m en la que 1 representa tierra y 0 agua, asumiendo que hay dos islas en el mapa,
+retorna el tamaño del puente más corto para conectar ambas islas.
 
-Recuerda que una isla está rodeada de agua y se forma conectando celdas con tierra adyacentes. Puedes suponer que los cuatro bordes de la cuadrícula están rodados de agua.
+Recuerda que una isla está rodeada de agua y se forma conectando celdas con tierra adyacentes. 
+Puedes suponer que los cuatro bordes de la cuadrícula están rodados de agua.
 
 Ejemplo 1:
 
@@ -34,31 +36,33 @@ Bridge().shortestBridge(mapa)
 2
 """
 
-
 class Bridge:
   def __init__(self) -> None:
     self.map = None
-    self.bridge = 0
+    self.bridge_lenght = None
     
   def shortestBridge(self, map):
       self.map = map
+      self.bridge_lenght = len(map[0])
       for y in range(len(map)):
         for x in range(len(self.map[0])):
-          if self.map[y][x] == 1 and x + 1 < len(self.map[0]) and self.map[y][x + 1] == 1:
-            self.dfs(x, y)
-      return self.bridge
+          if self.map[y][x] == 1:
+            result = self.dfs(x, y)
+            if result < self.bridge_lenght :
+               self.bridge_lenght = result
+      return self.bridge_lenght 
   
   def dfs(self, y, x):
-      if y < len(self.map) and x < len(self.map[0]):
-        for x in range(len(self.map[0]) - x):
-            if self.map[y][x] == 0 and x + 1 < len(self.map[0]) and self.map[y][x + 1] == 1:
-              self.bridge += 1
-        for y in range(len(self.map) - y):
-            if self.map[y][x] == 0 and y + 1 < len(self.map) and self.map[y + 1][x] == 1:
-              self.bridge += 1
-      return 
+      long = len(self.map[y])
+      if x < len(self.map[0]):
+        water_cell = 0
+        for x in range(len(self.map[0])):
+           if self.map[y][x] == 0:
+              water_cell += 1
+              if (x + 1) < long and self.map[y][x + 1] == 1:
+                return water_cell 
+      return long
             
-"""
 mapa = [
   [1,1,1,1,1],
   [1,0,0,0,1],
@@ -68,7 +72,7 @@ mapa = [
 ]
 response = Bridge().shortestBridge(mapa)
 print(response)
-"""
+
 
 mapa = [
   [1,1,0,0,1],
@@ -77,5 +81,6 @@ mapa = [
   [1,0,0,0,1],
   [1,0,0,1,1],
 ]
+
 response = Bridge().shortestBridge(mapa)
 print(response)
