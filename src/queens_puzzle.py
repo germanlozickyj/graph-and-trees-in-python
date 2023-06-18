9fe47f7bdf1b76fdb8f66761f5dec8a952247cc5
mrgerman <germanlozickyj2003@gmail.com>
2023-06-17 23:07:30 -0300

    doing queens puzzle

---
+++ src/queens_puzzle.py
@@ -0,0 +1,18 @@
+"""
+El rompecabezas de n reinas es el problema de colocar n reinas en un tablero de ajedrez de n x n 
+de manera que ninguna de ellas se ataque mutuamente.
+Dado un número entero n, devuelva todas las soluciones distintas del rompecabezas de n reinas. 
+Puede devolver la respuesta en cualquier orden. Cada solución contiene una configuración del tablero 
+distinta de la colocación de n reinas, donde “Q” y “.” indican una reina y un espacio vacío, respectivamente.
+"""
+
+"""
+Entrada: n = 4
+Salida: [[".Q…","…Q",“Q…”,"…Q."],["…Q.",“Q…”,"…Q",".Q…"]]
+Explicación: Existen dos soluciones distintas al rompecabezas de las 4 reinas como se muestra arriba
+"""
+def queens_puzzle(n : int):
+    pass
+
+n = 4
+result = queens_puzzle(n)
