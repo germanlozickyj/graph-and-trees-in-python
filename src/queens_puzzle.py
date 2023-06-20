16640884d1cb2aa7781f2e38060ee7eaff06361b
mrgerman <germanlozickyj2003@gmail.com>
2023-06-20 17:53:30 -0300

    doing

--- src/queens_puzzle.py
+++ src/queens_puzzle.py
@@ -21,19 +21,39 @@ Salida: [[
 Explicación: Existen dos soluciones distintas al rompecabezas de las 4 reinas como se muestra arriba
 """
 def backtrack_queens_puzzle(n : int):
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    result = dfs(n, current_collection=board)
+    #board = [[0 for _ in range(n)] for _ in range(n)]
+    result = dfs(n)
     return result
 
-def dfs(n, pointer = 0, collection_cache = [], current_collection = [], solutions = []):
-    collection_cache[pointer] = current_collection
-    current_collection[0][pointer] = 1
-    tmp_board = write_kill_cell(n, pointer, current_collection)
+def dfs(n, y = 0, x = 0, collection_cache = {}, current_collection = {}, solutions = []):
+    dict_kill_cell = kill_cell(n, 2, y)
+    collection_cache[str(y) + str(x)] = dict_kill_cell
 
-def write_kill_cell(n, x, board, y = 0):    
-    if n == x and n == y :
-        return board
 
+def kill_cell(n, x, y):    
+    hash = {}
+    y_increment = y
+    y_decrement = y
+    x_increment = x
+    x_decrement = x
+    
+    for movement in range(n - 2) :
+        if y_decrement >= movement and x_decrement >= movement :
+            hash[str(y_decrement) + str(x_decrement)] = 3
+
+        if y_increment >= movement and x_increment >= movement :
+            hash[str(y_increment) + str(x_increment)] = 3
+        
+        if y_increment >= movement and x_decrement >= movement :
+            hash[str(y_increment) + str(x_decrement)] = 3
+        
+        if y_decrement >= movement and x_increment >= movement :
+            hash[str(y_decrement) + str(x_increment)] = 3
+        y_increment += 1
+        x_increment += 1
+        y_decrement -= 1
+        x_decrement -= 1
+    return hash
 
 n = 4
 result = backtrack_queens_puzzle(n)
