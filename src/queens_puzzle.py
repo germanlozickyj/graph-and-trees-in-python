06866241487cbea63f282f951a9a09a16530d72f
mrgerman <germanlozickyj2003@gmail.com>
2023-06-21 22:34:31 -0300

    doing

--- src/queens_puzzle.py
+++ src/queens_puzzle.py
@@ -4,14 +4,7 @@ de manera que ninguna de ellas se ataque mutuamente.
 Dado un número entero n, devuelva todas las soluciones distintas del rompecabezas de n reinas. 
 Puede devolver la respuesta en cualquier orden. Cada solución contiene una configuración del tablero 
 distinta de la colocación de n reinas, donde “Q” y “.” indican una reina y un espacio vacío, respectivamente.
-[
-    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]], 
-    [
-        [1, 3, 3, 3], 
-        [3, 3, 0, 0], 
-        [3, 0, 3, 0], 
-        [3, 0, 0, 3]]
-]
+
 """
 
 """
@@ -28,59 +21,68 @@ Salida: [[
     ]]
 Explicación: Existen dos soluciones distintas al rompecabezas de las 4 reinas como se muestra arriba
 """
+
 import copy
 
-def backtrack_queens_puzzle(n: int):
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    cache_board = [copy.deepcopy(board)]
-    result = dfs(n, current_board=board, cache_board=cache_board)
-    return result
+class Solution :
+    def __init__(self, n : int) -> None:
+        board = [[0 for _ in range(n)] for _ in range(n)]
+        self.n = n
+        self.y = 0
+        self.x = 0
+        self.cache_board = board
+        self.current_board = board
+        self.solutions = []  
 
+    def backtrack_queens_puzzle(self):
+        result = self.dfs()
+        return result
 
-def dfs(n, y=0, x=0, cache_board=[], current_board={}, solutions=[]):
-    tmp_collect = kill_cell(n, y, x, current_board)
+    def dfs(self):
+        tmp_board = self.kill_cell(self.current_board)
+        self.current_board = tmp_board
+        tmp_cache = [self.cache_board]
+        self.cache_board = tmp_cache.append(tmp_board)
 
-    for cell in range(n):
-        if tmp_collect[y + 1][cell] == 0:
-            current_board[y][x] = 1
-            cache_board.append(copy.deepcopy(current_board))
-            current_board = copy.deepcopy(tmp_collect)
+        self.y += 1
 
-            return dfs(n, y + 1, cell, cache_board, current_board, solutions)
+        for x in range(self.n) :
+            if self.current_board[self.y][x] == 0 :
+                return self.dfs()
+            
+        return self.cache_board
+    #no visited 0 current 1 visited 2 kill 3
 
-    current_board = copy.deepcopy(cache_board.pop())
-    current_board[y][x] = 2
+    def kill_cell(self, board):
+        y_increment = self.y
+        y_decrement = self.y
+        x_increment = self.x
+        x_decrement = self.x
 
-    return dfs(n, y, x + 1, cache_board, current_board, solutions)    
+        for movement in range(self.n) :
+            board[self.y][movement] = 3
+            board[movement][self.x] = 3
 
+            if y_decrement >= self.n and x_decrement >= self.n and board[y_decrement][x_decrement] == 3 or board[y_decrement][x_decrement] == 0:
+                board[y_decrement][x_decrement] = 3
 
-def kill_cell(n, y, x, board):
-    y_increment = y
-    y_decrement = y
-    x_increment = x
-    x_decrement = x
+            if y_increment < self.n and x_increment < self.n and board[y_increment][x_increment] == 3 or board[y_increment][x_increment] == 0:
+                board[y_increment][x_increment] = 3
 
-    for movement in range(n) :
-        board[y][movement] = 3
-        board[movement][x] = 3
-        if y_decrement >= n and x_decrement >= n and board[y_decrement][x_decrement] != 1:
-            board[y_decrement][x_decrement] = 3
+            if y_increment < self.n and x_decrement >= 0 and board[y_increment][x_decrement] == 3 or board[y_increment][x_decrement] == 0:
+                board[y_increment][x_decrement] = 3
+            
+            if y_decrement >= self.n and x_increment >= self.n and board[y_decrement][x_increment] == 3 or board[y_decrement][x_increment] == 0:
+                board[y_decrement][x_increment] = 3
+            y_increment += 1
+            x_increment += 1
+            y_decrement -= 1
+            x_decrement -= 1
 
-        if y_increment < n and x_increment < n and board[y_increment][x_increment] != 1:
-            board[y_increment][x_increment] = 3
-        
-        if y_increment <= n and x_decrement >= 0 and board[y_increment][x_decrement] != 1:
-            board[y_increment][x_decrement] = 3
-        
-        if y_decrement >= n and x_increment >= n and board[y_decrement][x_increment] != 1:
-            board[y_decrement][x_increment] = 3
-        y_increment += 1
-        x_increment += 1
-        y_decrement -= 1
-        x_decrement -= 1
+        board[self.y][self.x] = 1
 
-    return board
+        return board
 
 n = 4
-result = backtrack_queens_puzzle(n)
+result = Solution(n).backtrack_queens_puzzle()
 print(result)
