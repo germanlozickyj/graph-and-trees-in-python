0c2080ea6696c396945d27af3c8575241e6995f3
mrgerman <germanlozickyj2003@gmail.com>
2023-06-22 23:03:45 -0300

    rework

--- src/queens_puzzle.py
+++ src/queens_puzzle.py
@@ -27,76 +27,23 @@ Salida: [[
 Explicación: Existen dos soluciones distintas al rompecabezas de las 4 reinas como se muestra arriba
 """
 
-import copy
-
 class Solution:
-    def __init__(self, n: int) -> None:
-        board = [[0 for _ in range(n)] for _ in range(n)]
-        self.n = n
-        self.y = 0
-        self.x = 0
-        self.cache_board = copy.deepcopy(board)
-        self.current_board = copy.deepcopy(board)
-        self.solutions = []
-
-    def backtrack_queens_puzzle(self):
-        result = self.dfs()
+    def backtrack_queens_puzzle(self, n):
+        result = self.dfs(n)
         return result
-
-    def dfs(self):        
-        tmp_board = self.kill_cell(copy.deepcopy(self.current_board))
-        self.current_board = copy.deepcopy(tmp_board)
-        self.y += 1
-
-        for x in range(n - 1):
-            if self.current_board[self.y][x] == 0:
-                self.cache_board.append(copy.deepcopy(self.current_board))
-                self.x = x
-                return self.dfs()
         
-        #no hay espacios vacios para la reina
-        last_board = copy.deepcopy(self.cache_board).pop()
-        last_board[self.y - 1][self.x] = 2
-        self.current_board = copy.deepcopy(last_board)
-        #guardar en cache ultima casilla visitada y dada de baja
-        self.cache_board.append(copy.deepcopy(self.current_board))
-        self.y -= 1
-        return self.dfs()
-        
-    def kill_cell(self, board):
-        y_increment = self.y
-        y_decrement = self.y
-        x_increment = self.x
-        x_decrement = self.x
+    def dfs(self, n):        
+        col = set() 
+        positive = set() # r + c 
+        negative = set() # r - c
+        response = []
+        board = [["."] * n for i in range(n)]
 
-        for movement in range(self.n) :
-            board[self.y][movement] = 3
-            board[movement][self.x] = 3
-
-            if y_decrement >= self.n and x_decrement >= self.n :
-                if board[y_decrement][x_decrement] == 3 or board[y_decrement][x_decrement] == 0:
-                    board[y_decrement][x_decrement] = 3
-                
-            if y_increment < self.n and x_increment < self.n :
-                if board[y_increment][x_increment] == 3 or board[y_increment][x_increment] == 0 :
-                    board[y_increment][x_increment] = 3
-
-            if y_increment < self.n and x_decrement >= 0:
-                if board[y_increment][x_decrement] == 3 or board[y_increment][x_decrement] == 0:
-                    board[y_increment][x_decrement] = 3
-            
-            if y_decrement >= self.n and x_increment >= self.n:
-                if board[y_decrement][x_increment] == 3 or board[y_decrement][x_increment] == 0:
-                    board[y_decrement][x_increment] = 3
-            y_increment += 1
-            x_increment += 1
-            y_decrement -= 1
-            x_decrement -= 1
-
-        board[self.y][self.x] = 1
-
-        return board
+        def backtrack(y) :
+            if y == n:
+                copy = ["".join(row) for row in board]
+                response.append(copy)
 
 n = 4
-result = Solution(n).backtrack_queens_puzzle()
+result = Solution().backtrack_queens_puzzle(n)
 print(result)
